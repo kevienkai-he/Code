c3ee1ce3ac7174877bf9f83a44558bfea44c2378
kevienkai <hkqkcp@163.com>
2022-01-03 15:29:02 +0800

    2022.1.3 更新

--- Ajax/封装自己的Ajax函数/js/Ajax.js
+++ Ajax/封装自己的Ajax函数/js/Ajax.js
@@ -1,12 +1,32 @@
-function  resolveData(data){
+function resolveData(data){
     let arr = [];
     for(let k in data){
-        let str = k + '=' + data[k];
-        arr.push(str)
+        let str = k + "=" + data[k];
+        arr.push(str);
+    }
+    return arr.join("&");
+}
+function myAjax(option){
+    /*1、创建xhr对象*/
+    let xhr = new XMLHttpRequest();
+    //返回处理后的数据
+    let qs = resolveData(option.data);
+    if(option.method.toUpperCase() === 'GET'){
+        xhr.open(option.method, option.url + "?" + qs);
+        xhr.send()
+    }
+    else if(option.method.toUpperCase() === 'POST'){
+        xhr.open(option.method, option.url);
+        xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+        xhr.send(qs);
     }
 
-    return arr.join("&")
-}
+    xhr.onreadystatechange = function (){
+        if(xhr.readyState === 4 && xhr.status === 200){
+            let result = JSON.parse(xhr.responseText);
 
-let res = resolveData({name:"kevienkai", age:29});
-console.log(res)
+            option.success(result);
+        }
+    }
+    /**/
+}
